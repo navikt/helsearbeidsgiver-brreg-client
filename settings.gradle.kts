fbf1d2d515c5f7e748253f3097e2eadd2128113e
Morten Laukvik <morten.laukvik@nav.no>
2022-05-19 08:42:29 +0200

    Brreg client

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "brreg-client"
+
+pluginManagement {
+    val jvmVersion: String by settings
+    plugins {
+        kotlin("jvm") version jvmVersion
+        kotlin("plugin.serialization") version jvmVersion
+        id("org.jmailen.kotlinter")
+        id("maven-publish")
+    }
+}
