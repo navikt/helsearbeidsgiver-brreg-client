853ee57d7980045d469e12251261d97b5b689bee
Mikael Bjerga <mikael.bjerga@nav.no>
2025-06-04 12:32:01 +0200

    Legg til retry og timeout

--- src/main/kotlin/no/nav/helsearbeidsgiver/brreg/HttpUtils.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/brreg/HttpUtils.kt
@@ -3,6 +3,8 @@ package no.nav.helsearbeidsgiver.brreg
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.apache5.Apache5
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import no.nav.helsearbeidsgiver.utils.json.jsonConfig
@@ -15,5 +17,22 @@ internal fun HttpClientConfig<*>.customize() {
 
     install(ContentNegotiation) {
         json(jsonConfig)
+    }
+
+    install(HttpRequestRetry) {
+        retryOnException(
+            maxRetries = 5,
+            retryOnTimeout = true,
+        )
+        constantDelay(
+            millis = 500,
+            randomizationMs = 500,
+        )
+    }
+
+    install(HttpTimeout) {
+        connectTimeoutMillis = 500
+        requestTimeoutMillis = 500
+        socketTimeoutMillis = 500
     }
 }
--- src/test/kotlin/no/nav/helsearbeidsgiver/brreg/MockBrregClient.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/brreg/MockBrregClient.kt
@@ -1,5 +1,7 @@
 package no.nav.helsearbeidsgiver.brreg
 
+import io.kotest.core.test.testCoroutineScheduler
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
@@ -8,19 +10,28 @@ import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.headersOf
 import io.mockk.every
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import no.nav.helsearbeidsgiver.utils.cache.LocalCache
 import no.nav.helsearbeidsgiver.utils.test.mock.mockStatic
 import kotlin.time.Duration
 
-fun mockBrregClient(
-    status: HttpStatusCode,
-    content: String = "",
-): BrregClient {
-    val mockEngine = MockEngine {
-        respond(
-            content = content,
-            status = status,
-            headers = headersOf(HttpHeaders.ContentType, ContentType.Application.Json.toString()),
+@OptIn(ExperimentalStdlibApi::class, ExperimentalCoroutinesApi::class)
+fun mockBrregClient(vararg responses: Pair<HttpStatusCode, String>): BrregClient {
+    val mockEngine = MockEngine.create {
+        reuseHandlers = false
+        requestHandlers.addAll(
+            responses.map { (status, content) ->
+                {
+                    if (content == "timeout") {
+                        dispatcher.shouldNotBeNull().testCoroutineScheduler.advanceTimeBy(600)
+                    }
+                    respond(
+                        content = content,
+                        status = status,
+                        headers = headersOf(HttpHeaders.ContentType, ContentType.Application.Json.toString()),
+                    )
+                }
+            },
         )
     }
 
